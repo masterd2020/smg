ea443f9cd0a9dff8193c84cad4ddeb6ef5493d43
Fasasi Toheeb <tohebayomide11@gmail.com>
2022-05-15 01:18:58 +0100

    feat: refactor recent post and recent user tab

--- src/components/reusable/RecentUsersTab.js
+++ src/components/reusable/RecentUsersTab.js
@@ -11,86 +11,75 @@ const RecentUsersTab = () => {
       {/** Left content */}
       <div className='lg:w-4/6'>
         {/** Header tab */}
-        <div className='lg:w-11/12 lg:flex lg:gap-3 lg:text-sm lg:mb-3'>
-          <div className='lg:w-1/4 lg:flex lg:items-center'>
-            <Checkbox size='small' />
-            <span>ID</span>
-            <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-          </div>
-          <div className='lg:w-1/4 lg:flex lg:items-center'>
-            <span>TITLE</span>
-            <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-          </div>
-          <div className='lg:w-4/5 lg:flex lg:items-center'>
-            <span>NAME</span>
-            <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-          </div>
-          <div className='lg:w-1/4 lg:flex lg:items-center'>
-            <span>ID</span>
-            <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-          </div>
-          <div className='lg:w-1/4 lg:flex lg:items-center'>
-            <span>PHONE</span>
-            <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-          </div>
-        </div>
-
+        <RecentUserHeader />
 
         {/** Header tab content */}
         <div className="lg:h-96 lg:overflow-y-scroll">
-          <div className='lg:w-full lg:rounded-md lg:shadow-md lg:bg-white lg:flex lg:items-center lg:py-2 lg:text-xsm lg:mb-4'>
-            <div className='lg:w-full lg:flex lg:items-center lg:gap-3'>
-              <div className='lg:w-1/4 lg:flex lg:items-center'>
-                <Checkbox size='small' />
-                <p>3qradsf4</p>
-              </div>
-              <p className='lg:w-1/4'>Mr</p>
-              <div className='lg:w-4/5 lg:flex lg:items-center lg:gap-2 lg:text-xs'>
-                <div className='lg:w-10 lg:h-10 lg:rounded-full lg:overflow-hidden'>
-                  <img src={image} alt='user pic' />
-                </div>
-                <div className=''>
-                  <p className='lg:text-indigo-300'>Kehinde Adebisi</p>
-                  <p className=''>@Adebisi</p>
-                </div>
-              </div>
-              <p className='lg:w-1/4'>32rfafef333...</p>
-              <p className='lg:w-1/4'>Oct 2003</p>
-            </div>
-            <div className='lg:w-14'>
-              <MoreVertIcon />
-            </div>
-          </div>
-  
-          <div className='lg:w-full lg:rounded-md lg:shadow-md lg:bg-white lg:flex lg:items-center lg:py-2 lg:text-xsm lg:mb-4'>
-            <div className='lg:w-full lg:flex lg:items-center lg:gap-3'>
-              <div className='lg:w-1/4 lg:flex lg:items-center'>
-                <Checkbox size='small' />
-                <p>3qradsf4</p>
-              </div>
-              <p className='lg:w-1/4'>Mr</p>
-              <div className='lg:w-4/5 lg:flex lg:items-center lg:gap-2 lg:text-xs'>
-                <div className='lg:w-10 lg:h-10 lg:rounded-full lg:overflow-hidden'>
-                  <img src={image} alt='user pic' />
-                </div>
-                <div className=''>
-                  <p className='lg:text-indigo-300'>Kehinde Adebisi</p>
-                  <p className=''>@Adebisi</p>
-                </div>
-              </div>
-              <p className='lg:w-1/4'>32rfafef333...</p>
-              <p className='lg:w-1/4'>Oct 2003</p>
-            </div>
-            <div className='lg:w-14'>
-              <MoreVertIcon />
-            </div>
-          </div>
+          <SingleUserCard />
+          <SingleUserCard />
+          <SingleUserCard />
         </div>
       </div>
 
       {/** Right content */}
       <UserDetailCard />
       
+    </div>
+  )
+}
+
+const RecentUserHeader = () => {
+  return (
+    <div className='lg:w-11/12 lg:flex lg:gap-3 lg:text-sm lg:mb-3'>
+      <div className='lg:w-1/4 lg:flex lg:items-center'>
+        <Checkbox size='small' />
+        <span>ID</span>
+        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+      </div>
+      <div className='lg:w-1/4 lg:flex lg:items-center'>
+        <span>TITLE</span>
+        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+      </div>
+      <div className='lg:w-4/5 lg:flex lg:items-center'>
+        <span>NAME</span>
+        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+      </div>
+      <div className='lg:w-1/4 lg:flex lg:items-center'>
+        <span>ID</span>
+        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+      </div>
+      <div className='lg:w-1/4 lg:flex lg:items-center'>
+        <span>PHONE</span>
+        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+      </div>
+    </div>
+  )
+}
+
+const SingleUserCard = () => {
+  return (
+    <div className='lg:w-full lg:rounded-md lg:shadow-md lg:bg-white lg:flex lg:items-center lg:py-2 lg:text-xsm lg:mb-4'>
+      <div className='lg:w-full lg:flex lg:items-center lg:gap-3'>
+        <div className='lg:w-1/4 lg:flex lg:items-center'>
+          <Checkbox size='small' />
+          <p>3qradsf4</p>
+        </div>
+        <p className='lg:w-1/4'>Mr</p>
+        <div className='lg:w-4/5 lg:flex lg:items-center lg:gap-2 lg:text-xs'>
+          <div className='lg:w-10 lg:h-10 lg:rounded-full lg:overflow-hidden'>
+            <img src={image} alt='user pic' />
+          </div>
+          <div className=''>
+            <p className='lg:text-indigo-300'>Kehinde Adebisi</p>
+            <p className=''>@Adebisi</p>
+          </div>
+        </div>
+        <p className='lg:w-1/4'>32rfafef333...</p>
+        <p className='lg:w-1/4'>Oct 2003</p>
+      </div>
+      <div className='lg:w-14'>
+        <MoreVertIcon />
+      </div>
     </div>
   )
 }
