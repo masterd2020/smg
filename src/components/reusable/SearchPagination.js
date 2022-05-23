62a450150485bd8b4d1f704d543bd7257698e549
Fasasi Toheeb <tohebayomide11@gmail.com>
2022-05-23 13:41:08 +0100

    feat: complete search post api integration

--- src/components/reusable/SearchPagination.js
+++ src/components/reusable/SearchPagination.js
@@ -1,17 +1,36 @@
+import {useGetPostsQuery} from '../../features/post/postSlice'
+
+import Loading from './Loading'
+
 const SearchPagination = () => {
+    let {data, isFetching} = useGetPostsQuery()
+    const len = data?.total/data?.limit
+
   return (
-    <div className="lg:flex lg:gap-2 lg:items-center lg:bg-white lg:rounded-md lg:shadow-md lg:my-2 lg:p-2 lg:w-72">
+    <div className="lg:grid lg:grid-cols-12 lg:gap-2 lg:items-center lg:bg-white lg:rounded-md lg:shadow-md lg:my-2 lg:p-2 lg:w-full">
       <Button text='Prev' noBorder={true} />
       <Button text='1' active={true}/>
-      <Button text='2' />
-      <Button text='3' />
+      {
+        isFetching ? <Loading size='2xl'/> : <ButtonList len={len}/>
+      }
       <Button text='Next' />
     </div>
   )
 }
 
+const ButtonList = ({len}) => {
+
+  return(
+    <>
+      {
+        Array.from(Array(Math.floor(len)).keys()).map((i) => <Button key={i} text={i+1} />)
+      }
+    </>
+  )
+}
+
 const Button = ({noBorder, text, active}) => {
-  let defaultButtonStyle = 'lg:px-3 lg:py-1 lg:rounded-md lg:border-2 lg:text-indigo-300 lg:cursor-pointer'
+  let defaultButtonStyle = 'lg:px-3 lg:py-1 lg:rounded-md lg:border-2 lg:text-center lg:text-indigo-300 lg:cursor-pointer lg:hover:bg-indigo-600'
   defaultButtonStyle = active && active === true ? defaultButtonStyle.concat(' ').concat('lg:bg-indigo-300 lg:text-gray-300 lg:border-none') : defaultButtonStyle
 
   return (
--- src/components/reusable/SearchResults.js
+++ src/components/reusable/SearchResults.js
@@ -2,86 +2,102 @@ import Checkbox from '@mui/material/Checkbox';
 import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import {useGetPostsQuery} from '../../features/post/postSlice'
 
-import image from '../../image/user.png';
+import Loading from './Loading'
 
-const RecentUsersTab = () => {
+import defaultImage from '../../image/user.png';
+import handleError from '../../lib/handleError'
+
+const SearchPostResult = () => {
+  let {data, isFetching, isError, error} = useGetPostsQuery()
+
+  handleError(isError, error);
+
   return (
     <div className='lg:flex lg:gap-4 lg:mt-4'>
       {/** Left content */}
       <div className='lg:w-full'>
         {/** Header tab */}
-        <RecentUserHeader />
+        <RecentPostHeader />
 
         {/** Header tab content */}
-        <div className="lg:h-96 lg:overflow-y-scroll">
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
+        <div className="lg:h-100 lg:overflow-y-scroll">
+          {
+            isFetching ? <Loading size='2xl'/> : <PostList  data={data}/>
+          }
         </div>
       </div>
-
     </div>
   )
 }
 
-const RecentUserHeader = () => {
+const RecentPostHeader = () => {
   return (
-    <div className='lg:w-11/12 lg:flex lg:gap-3 lg:text-sm lg:mb-3'>
-      <div className='lg:w-1/4 lg:flex lg:items-center'>
-        <Checkbox size='small' />
-        <span>ID</span>
-        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-      </div>
-      <div className='lg:w-1/2 lg:flex lg:items-center'>
-        <span>OWNER</span>
-        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-      </div>
-      <div className='lg:w-1/4 lg:flex lg:items-center'>
-        <span>TITLE</span>
-        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-      </div>
-      <div className='lg:w-1/4 lg:flex lg:items-center'>
-        <span>LINKS</span>
-        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-      </div>
-      <div className='lg:w-1/4 lg:flex lg:items-center'>
-        <span>PUBLISHED DATE</span>
-        <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
-      </div>
-    </div>
-  )
-}
-
-const SingleUserCard = () => {
-  return (
-    <div className='lg:w-full lg:rounded-md lg:shadow-md lg:bg-white lg:flex lg:items-center lg:py-3 lg:text-sm lg:mb-4'>
-      <div className='lg:w-full lg:flex lg:items-center lg:gap-3'>
+    <div className='w-97-p'>
+      <div className='lg:w-11/12 lg:flex lg:gap-3 lg:text-sm lg:mb-3'>
         <div className='lg:w-1/4 lg:flex lg:items-center'>
           <Checkbox size='small' />
-          <p>3qradsf4</p>
+          <span>ID</span>
+          <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+        </div>
+        <div className='lg:w-1/2 lg:flex lg:items-center'>
+          <span>OWNER</span>
+          <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+        </div>
+        <div className='lg:w-1/4 lg:flex lg:items-center'>
+          <span>TITLE</span>
+          <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+        </div>
+        <div className='lg:w-1/4 lg:flex lg:items-center'>
+          <span>LINKS</span>
+          <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+        </div>
+        <div className='lg:w-1/4 lg:flex lg:items-center'>
+          <span>PUBLISHED DATE</span>
+          <UnfoldMoreIcon style={{fontSize: '.8rem'}}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const PostList = ({data, setCurrentPostId}) => {
+  return (
+    <>
+      {
+        data?.data.map((post) => <SinglePostCard key={post.id} post={post} setCurrentPostId={setCurrentPostId}/>)
+      }
+    </>
+  )
+}
+
+
+const SinglePostCard = ({post}) => {
+  const {id, image, likes, owner: {firstName, lastName, title}, publishDate} = post;
+
+  return (
+    <div className='lg:w-full lg:rounded-md lg:shadow-md lg:bg-white lg:flex lg:items-center lg:py-3 lg:text-sm lg:mb-4'>
+      <div className='lg:w-11/12 lg:flex lg:items-center lg:gap-3'>
+        <div className='lg:w-1/4 lg:flex lg:items-center'>
+          <Checkbox size='small' />
+          <p>{id.slice(0, 5)}</p>
         </div>
         <div className='lg:w-1/2 lg:flex lg:items-center lg:gap-2 lg:text-xs'>
           <div className='lg:w-10 lg:h-10 lg:rounded-full lg:overflow-hidden'>
-            <img src={image} alt='user pic' />
+            <img src={image || defaultImage} alt='user pic' />
           </div>
           <div className=''>
-            <p className='lg:text-indigo-300'>Kehinde Adebisi</p>
-            <p className=''>@Adebisi</p>
+            <p className='lg:text-indigo-300'>{firstName} {lastName}</p>
+            <p className=''>@{firstName}</p>
           </div>
         </div>
-        <p className='lg:w-1/4'>hey watsapp</p>
+        <p className='lg:w-1/4'>{title}</p>
         <div className='lg:w-1/4 lg:flex lg:items-center lg:gap-2 lg:text-gray-400'>
           <ThumbUpAltOutlinedIcon size='small' />
-          <p>32</p>
+          <p>{likes}</p>
         </div>
-        <p className='lg:w-1/4'>Oct 2003</p>
+        <p className='lg:w-1/4'>{new Date(publishDate).toDateString()}</p>
       </div>
       <div className='lg:w-20 lg:text-right lg:pr-4'>
         <MoreVertIcon />
@@ -90,4 +106,4 @@ const SingleUserCard = () => {
   )
 }
 
-export default RecentUsersTab
+export default SearchPostResult
